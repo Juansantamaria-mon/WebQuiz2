db3eee599c4d681c9cac02dae5d4b2bb232ca58b
Juansantamaria-mon <juan.santamaria2005@gmail.com>
2026-09-07 15:53:13 -0500

    Quiz Spring Boot y JPA - Producto

--- src/main/java/com/example/demo/controller/ProductoController.java
+++ src/main/java/com/example/demo/controller/ProductoController.java
@@ -2,7 +2,6 @@ package com.example.demo.controller;
 
 import com.example.demo.entity.Producto;
 import com.example.demo.service.ProductoService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,18 +12,21 @@ import java.util.List;
 @RequestMapping("/productos")
 public class ProductoController {
 
-    @Autowired
-    private ProductoService productoService;
+    private final ProductoService productoService;
+
+    public ProductoController(ProductoService productoService) {
+        this.productoService = productoService;
+    }
 
     @PostMapping
     public ResponseEntity<Producto> registrarProducto(@RequestBody Producto producto) {
         Producto nuevoProducto = productoService.registrarProducto(producto);
-        return new ResponseEntity<>(nuevoProducto, HttpStatus.CREATED);
+        return ResponseEntity.status(HttpStatus.CREATED).body(nuevoProducto);
     }
 
     @GetMapping("/buscar/{categoria}")
     public ResponseEntity<List<Producto>> buscarPorCategoria(@PathVariable String categoria) {
         List<Producto> productos = productoService.buscarPorCategoria(categoria);
-        return new ResponseEntity<>(productos, HttpStatus.OK);
+        return ResponseEntity.ok(productos);
     }
 }
